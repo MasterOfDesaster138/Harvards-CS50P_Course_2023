7ca13e22b6c4f63ba57ab55dcfd24271734e67bb
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2023-05-29 18:08:56 +0200

    solved

--- Problem_Sets/Problemset_3/grocery/grocery.py
+++ Problem_Sets/Problemset_3/grocery/grocery.py
@@ -16,10 +16,41 @@
             prefixing each line with the number of times the user inputted that item. No need to pluralize the items. 
             Treat the user’s input case-insensitively.
 """
+def main():
+    grocery_list = get_input()
+    print_list(grocery_list)
+    
+
+def get_input() -> list:
+    grocery_list = []
+    while True:
+        try: 
+            # get user's input case-insensitively
+            grocery_input = input().strip()
+            # store every item in a list in uppercase
+            grocery_list.append(grocery_input.upper())
+        except KeyboardInterrupt:
+            return grocery_list
+            
+        
+def print_list(grocery_list: list):
+    """Outputs the user’s grocery list in all uppercase, sorted alphabetically by item, 
+    prefixing each line with the number of times the user inputted that item."""
+    # create a set of grocery_list for removing all duplicates
+    distinct_items = set(grocery_list)
+    # sort distinct items alphabetically 
+    item_list = sorted(list(distinct_items))
+    
+    # go through each item and prepare a string for output
+    for item in item_list:
+        # count the amount in grocery_list
+        amount = grocery_list.count(item)
+        print(f"{amount} {item}")
 
 
-
-
+if __name__ == '__main__':
+    main()
+    
 
 """"Here’s how to test your code manually:
 
