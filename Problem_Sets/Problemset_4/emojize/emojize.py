b3abbeed7ae9549685f89cf0076764498bfb12f4
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2023-06-01 21:33:03 +0200

    setup

---
+++ Problem_Sets/Problemset_4/emojize/emojize.py
@@ -0,0 +1,55 @@
+"""
+    Title: Problemset_3 - Emojize
+
+    Author: Emely Benke
+    
+    Created: 2023-06-01
+    
+    Version: 0.1
+    
+    Summary: 
+        Because emoji aren’t quite as easy to type as text, at least on laptops and desktops, 
+        some programs support “codes,” whereby you can type, for instance, 
+        :thumbs_up:, which will be automatically converted to 👍. 
+        Some programs additionally support aliases, whereby you can more succinctly type, 
+        for instance, :thumbsup:, which will also be automatically converted to 👍.
+
+        See carpedm20.github.io/emoji/all.html?enableList=enable_list_alias for a list of codes with aliases.
+
+        In a file called emojize.py, implement a program 
+        that prompts the user for a str in English 
+        and then outputs the “emojized” version of that str, 
+        converting any codes (or aliases) therein to their corresponding emoji.
+"""
+def main():
+    pass
+
+
+def get_input() -> str:
+    pass
+
+
+def translate_to_emoji(user_input: str) -> str:
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    
+
+""""Here’s how to test your code manually:
+
+Run your program with python emojize.py. Type :1st_place_medal: and press Enter. 
+Your program should output:
+    Output: 🥇
+
+Run your program with python emojize.py. Type :money_bag: and press Enter. 
+Your program should output:
+    Output: 💰
+
+Run your program with python emojize.py. Type :smile_cat: and press Enter. 
+Your program should output:
+    Output: 😸
+"""
+
+"""check50 cs50/problems/2022/python/emojize"""
