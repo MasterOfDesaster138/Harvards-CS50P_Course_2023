ff42d2973f4778577d243e5c3094be8cd5422e6b
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2023-06-03 11:26:57 +0200

    solved

--- Problem_Sets/Problemset_4/game/game.py
+++ Problem_Sets/Problemset_4/game/game.py
@@ -1,5 +1,5 @@
 """ 
-    Title: Problemset_4 - Adieu, Adieu
+    Title: Problemset_4 - Guessing Game
 
     Author: Emely Benke
     
@@ -15,11 +15,11 @@
 
             In a file called game.py, implement a program that:
 
-            Prompts the user for a level, 
+            Prompts the user for a level *n*, 
             . If the user does not input a positive integer, 
             the program should prompt again.
-            Randomly generates an integer between 1 and 
-            , inclusive, using the random module.
+            Randomly generates an integer between 1 and *n*
+            , inclusive *n* , using the random module.
             Prompts the user to guess that integer. 
             If the guess is not a positive integer, 
             the program should prompt the user again.
@@ -31,8 +31,87 @@
             the program should output Just right! and exit.
 
 """
+import sys, random
+
+
 def main():
-    pass
+    # prompts a level from the user
+    level = get_level()
+    # selects a random number between 1 and level
+    random_number = get_random_number(level)
+    # prompts for a guess and checks if guess matches random_number in a loop
+    while True:
+        guess = prompt_guess(level)
+        # checks if guess matches and returns a feedback for the user
+        print(check_guess(guess, random_number))
+
+
+def get_level() -> int:
+    """Prompts the Level from the User and excludes negative (or other datatypes than) integers.
+
+    Returns:
+        int: returns the max level for the random number
+    """
+    while True:
+        try: # prompting the max level of the random number
+            # only accepts a positive integer
+            level_input = int(input("Level: "))
+            if level_input > 0:
+                return level_input
+            
+        except:
+            pass
+
+
+def get_random_number(level: int) -> int:
+    """Returns a random number between 1 and inclusive level argument. 
+
+    Args:
+        level (int): the inclusive maximum of the range
+
+    Returns:
+        int: a random number between 1 and inclusive level
+    """
+    return random.randint(1, level)
+
+
+def prompt_guess(level: int) -> int:
+    """Prompts a positive integer from the user to guess the random number.
+
+    Args:
+        level (int): the max possible number
+
+    Returns:
+        int: the guess of the user as integer
+    """
+    while True:
+        try:
+            guess = int(input("Guess: "))
+            if 0 < guess < level+1:
+                return guess
+        
+        except:
+            pass
+
+
+def check_guess(guess: int, match: int) -> str:
+    """Checks if User's Guess matches the random Number and returns a feedback.
+    It returns a winner message or hints if the number is larger or smaller.
+
+    Args:
+        guess (int): the inputted guess from the user
+        match (int): the random number the user tries to guess
+
+    Returns:
+        str: a hint or a winning message for the user as feedback
+    """
+    if guess < match:
+        return "Too small!"
+    elif guess > match:
+        return "Too large!"
+    elif guess == match:
+        # close the game if the user has a match
+        return sys.exit("Just right!")
 
 
 if __name__ == '__main__':
