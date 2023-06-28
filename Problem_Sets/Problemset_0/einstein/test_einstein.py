3e273902babd5f278f8e3ddb80b4390865784fb6
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2023-06-28 12:32:19 +0200

    working directory cleaned up

--- Problem_Sets/Problemset_0/einstein/test_einstein.py
+++
@@ -1,9 +0,0 @@
-"""Testcases for the Einstein Problem."""
-# Import the function(s) we want to test
-from einstein import compute_energy, SPEED_OF_LIGHT
-
-def test_compute_energy():
-    assert compute_energy(1, SPEED_OF_LIGHT) == 90000000000000000
-    assert compute_energy(14, SPEED_OF_LIGHT) == 1260000000000000000
-    assert compute_energy(50, SPEED_OF_LIGHT) == 4500000000000000000
-
