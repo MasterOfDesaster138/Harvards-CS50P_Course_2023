8f9bcc30b29d8aaefb6579dbf72ae56ccc455bfd
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2023-07-10 22:42:46 +0000

    Tue, Jul 11, 2023, 12:42 AM +02:00

--- Problem_Sets/Problemset_7/watch/watch.py
+++ Problem_Sets/Problemset_7/watch/watch.py
@@ -72,7 +72,7 @@ def main():
 
 def parse(s: str) -> str:
     # Define the pattern for the Youtube-URL's
-    pattern = r'<iframe.*?src="(https?:\/\/(?:www\.)?youtube\.com(?:\/embed)?\/[a-zA-z0-9_-]+)".*?'
+    pattern = r'(?:https?://)?(?:www\.)?youtube\.com/embed/([^\s\"]+)'
 
     # Search for the pattern in the HTML-String
     match = re.search(pattern, s)
